fb24aa753f2bcaa46c09197eb665ee024b659d27
Nick Zana <me@nickzana.dev>
2021-11-12 20:35:11 -0500

    Add Collection as a Media Type
    Alias LocalizedString as String for better readability

--- src/media.rs
+++ src/media.rs
@@ -1,20 +1,21 @@
 use crate::Tag;
 use crate::{chapter::Chapter, episode::Episode, series::Series};
-use bamboo_metadata_util::{art::Art, id::Id, LocalizedString};
+use bamboo_metadata_util::{art::Art, id::Id, LocalizedString as String};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub enum Type {
+    Collection(Vec<Media>),
     Series(Series),
     Episode(Episode),
     Chapter(Chapter),
 }
 
 #[derive(Serialize, Deserialize)]
-struct Media {
+pub struct Media {
     pub identifiers: Vec<Id>,
     pub tags: Vec<Tag>,
-    pub title: LocalizedString,
+    pub title: String,
     // TODO: Consider making description a localized string to allow multiple langs
     pub description: Option<String>,
     pub art: Vec<Art>,
